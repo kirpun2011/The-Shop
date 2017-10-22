ea4b162ba757e96b9ef468ecef2cbe3f7880681b
kirpun2011 <kirpun2011@yandex.ru>
2017-10-22 22:13:57 +0300

    finish main part

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,28 @@
-// Your code here...
+Your code here...
+$(".color_one").on("click", function () {
+    $(this).toggleClass("active")
+    $(".color_two").removeClass("active")
+    $(".color_three").removeClass("active")
+    $(".photo_one").toggleClass("photo_one")
+    $(".photo_two_invisible").removeClass("photo_two")
+    $(".photo_three_invisible").removeClass("photo_three")
+});
+
+
+$(".color_two").on("click", function () {
+    $(this).toggleClass("active")
+    $(".color_one").removeClass("active")
+    $(".color_three").removeClass("active")
+    $(".photo_one").toggleClass("photo_one_invisible")
+    $(".photo_two_invisible").toggleClass("photo_two")
+    $(".photo_three_invisible").removeClass("photo_three")
+});
+
+$(".color_three").on("click", function () {
+    $(this).toggleClass("active")
+    $(".color_one").removeClass("active")
+    $(".color_two").removeClass("active")
+    $(".photo_three_invisible").toggleClass("photo_three")
+    $(".photo_one").toggleClass("photo_one_invisible")
+    $(".photo_two_invisible").removeClass("photo_two")
+});
