a2815fabd7577f2265a9281284bf19db36b4d5f3
kirpun2011 <kirpun2011@yandex.ru>
2017-10-29 19:39:20 +0300

    add changing colors

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,28 +1,58 @@
-Your code here...
-$(".color_one").on("click", function () {
-    $(this).toggleClass("active")
-    $(".color_two").removeClass("active")
-    $(".color_three").removeClass("active")
-    $(".photo_one").toggleClass("photo_one")
-    $(".photo_two_invisible").removeClass("photo_two")
-    $(".photo_three_invisible").removeClass("photo_three")
-});
+// ПЕРЕКЛЮЧЕНИЕ РАЗМЕРОВ
+alert("В субботу вечером только вернулся из Москвы с кейса, успел сделать переключение цветов и размеров. То, что пропустил про EJS и остальное, буду стараться нагонять. Пока так");
+
+sizeS.addEventListener("click", S);
+sizeM.addEventListener("click", M);
+sizeL.addEventListener("click", L);
+
+function S() {
+    sizeS.className = "size_style size_S active";
+    sizeM.className = "size_style size_M";
+    sizeL.className = "size_style size_L";
+}
+
+function M() {
+    sizeS.className = "size_style size_S";
+    sizeM.className = "size_style size_M active";
+    sizeL.className = "size_style size_L";
+}
+
+function L() {
+    sizeS.className = "size_style size_S";
+    sizeM.className = "size_style size_M";
+    sizeL.className = "size_style size_L active";
+}
 
 
-$(".color_two").on("click", function () {
-    $(this).toggleClass("active")
-    $(".color_one").removeClass("active")
-    $(".color_three").removeClass("active")
-    $(".photo_one").toggleClass("photo_one_invisible")
-    $(".photo_two_invisible").toggleClass("photo_two")
-    $(".photo_three_invisible").removeClass("photo_three")
-});
+// ПЕРЕКЛЮЧЕНИЕ ЦВЕТОВ
 
-$(".color_three").on("click", function () {
-    $(this).toggleClass("active")
-    $(".color_one").removeClass("active")
-    $(".color_two").removeClass("active")
-    $(".photo_three_invisible").toggleClass("photo_three")
-    $(".photo_one").toggleClass("photo_one_invisible")
-    $(".photo_two_invisible").removeClass("photo_two")
-});
+color_white.addEventListener("click", white);
+color_yellow.addEventListener("click", yellow);
+color_green.addEventListener("click", green);
+
+function white() {
+    color_white.className = "color_one active";
+    color_yellow.className = "color_two";
+    color_green.className = "color_three";
+    tshirtOne.className = "photo photo_one";
+    tshirtTwo.className = "photo photo_two_invisible";
+    tshirtThree.className = "photo photo_three_invisible";
+}
+
+function yellow() {
+    color_white.className = "color_one";
+    color_yellow.className = "color_two active";
+    color_green.className = "color_three";
+    tshirtOne.className = "photo photo_one_invisible";
+    tshirtTwo.className = "photo photo_two";
+    tshirtThree.className = "photo photo_three_invisible";
+}
+
+function green() {
+    color_white.className = "color_one";
+    color_yellow.className = "color_two";
+    color_green.className = "color_three active";
+    tshirtOne.className = "photo photo_one_invisible";
+    tshirtTwo.className = "photo photo_two_invisible";
+    tshirtThree.className = "photo photo_three";
+}
