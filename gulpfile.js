733ae36e5c5037512c7837183f1076b202a82d17
Vadim <kirpun2011@yandex.ru>
2017-11-09 00:15:07 +0300

    remove ejs from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,6 @@ const gulp         = require('gulp');
 const less         = require('gulp-less');
 const browserSync  = require('browser-sync');
 const autoprefixer = require('gulp-autoprefixer');
-const rename       = require('gulp-rename');
-const ejs          = require('gulp-ejs');
-const gutil        = require('gulp-util');
 
 // Автоперезагрузка при изменении файлов в папке `dist`:
 // Принцип: меняем файлы в `/src`, они обрабатываются и переносятся в `dist` и срабатывает автоперезагрузка.
@@ -40,9 +37,7 @@ gulp.task('js', () => {
 });
 
 gulp.task('html', () => {
-    gulp.src('src/index.ejs')
-    .pipe(ejs().on('error', gutil.log))
-    .pipe(rename('index.html'))
+    gulp.src('src/index.html')
         .pipe(gulp.dest('./dist'));
 });
 
@@ -50,7 +45,6 @@ gulp.task('html', () => {
 gulp.task('watch', () => {
     gulp.watch('src/less/**/*.less', ['styles']);
     gulp.watch('src/**/*.html', ['html']);
-    gulp.watch('src/**/*.ejs', ['html']);
     gulp.watch('src/img/**/*.*', ['img']);
     gulp.watch('src/js/**/*.*', ['js']);
 });
