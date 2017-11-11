9e72718cbb87c2bd916e7c0487dffa1a1ebb5a84
Vadim <kirpun2011@yandex.ru>
2017-11-11 14:55:18 +0300

    remade changing colors and photos, delete 2 and 3 photos with absolute

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,57 +1,47 @@
 // ПЕРЕКЛЮЧЕНИЕ РАЗМЕРОВ
 
-sizeS.addEventListener("click", S);
-sizeM.addEventListener("click", M);
-sizeL.addEventListener("click", L);
-
-function S() {
-    sizeS.className = "size_style size_S active";
-    sizeM.className = "size_style size_M";
-    sizeL.className = "size_style size_L";
+function changeSize(size) {
+    if (size === 's') {
+        sizeS.className = 'size_style size_S active';
+        sizeM.className = 'size_style size_M';
+        sizeL.className = 'size_style size_L';
+    } else if (size === 'm') {
+        sizeS.className = 'size_style size_S';
+        sizeM.className = 'size_style size_M active';
+        sizeL.className = 'size_style size_L';
+    } else if (size === 'l') {
+        sizeS.className = 'size_style size_S';
+        sizeM.className = 'size_style size_M';
+        sizeL.className = 'size_style size_L active';
+    }
 }
 
-function M() {
-    sizeS.className = "size_style size_S";
-    sizeM.className = "size_style size_M active";
-    sizeL.className = "size_style size_L";
-}
-
-function L() {
-    sizeS.className = "size_style size_S";
-    sizeM.className = "size_style size_M";
-    sizeL.className = "size_style size_L active";
-}
-
+document.getElementById('sizeList').addEventListener('click', ev => {
+    const size = ev.target.dataset['size'];
+    changeSize(size);
+})
 
 // ПЕРЕКЛЮЧЕНИЕ ЦВЕТОВ
 
-colorWhite.addEventListener("click", white);
-colorYellow.addEventListener("click", yellow);
-colorGreen.addEventListener("click", green);
+function changePicture(color) {
+    document.getElementById('productPicture').src = 'img/tshirts/tshirt_' + color + '.jpg';
 
-function white() {
-    colorWhite.className = "color_one active";
-    colorYellow.className = "color_two";
-    colorGreen.className = "color_three";
-    tshirtOne.className = "photo photo_one";
-    tshirtTwo.className = "photo photo_two_invisible";
-    tshirtThree.className = "photo photo_three_invisible";
+    if (color === 'white') {
+        colorWhite.className = 'color_one active';
+        colorYellow.className = 'color_two';
+        colorGreen.className = 'color_three';
+    } else if (color === 'yellow') {
+        colorWhite.className = 'color_one';
+        colorYellow.className = 'color_two active';
+        colorGreen.className = 'color_three';
+    } else if (color === 'green') {
+        colorWhite.className = 'color_one';
+        colorYellow.className = 'color_two';
+        colorGreen.className = 'color_three active';
+    }
 }
 
-function yellow() {
-    colorWhite.className = "color_one";
-    colorYellow.className = "color_two active";
-    colorGreen.className = "color_three";
-    tshirtOne.className = "photo photo_one_invisible";
-    tshirtTwo.className = "photo photo_two";
-    tshirtThree.className = "photo photo_three_invisible";
-}
-
-function green() {
-    colorWhite.className = "color_one";
-    colorYellow.className = "color_two";
-    colorGreen.className = "color_three active";
-    tshirtOne.className = "photo photo_one_invisible";
-    tshirtTwo.className = "photo photo_two_invisible";
-    tshirtThree.className = "photo photo_three";
-}
+document.getElementById('colorList').addEventListener('click', ev => {
+    const color = ev.target.dataset['color'];
+    changePicture(color);
+})
